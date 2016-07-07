5db8b959d0f43b7ae7a2f5f7208af913a8b29d72
Euler Santana <euler.santana@yahoo.com>
2016-07-07 00:59:31 -0300

    Start no app

---
+++ app/doaae/www/js/controllers.js
@@ -0,0 +1,64 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $cordovaGeolocation) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state, $ionicTabsDelegate) {
+  $scope.data = {};
+
+   $scope.login = function() {
+       LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+         console.log(data);
+           $state.go('tab.dash');
+       }).error(function(data) {
+           var alertPopup = $ionicPopup.alert({
+               title: 'Login failed!',
+               template: 'Please check your credentials!'
+           });
+       });
+   }
+
+   $scope.selectTabWithIndex = function(index) {
+      $ionicTabsDelegate.select(index);
+    }
+})
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
